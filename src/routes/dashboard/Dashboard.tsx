7df20986201c527c762ce47ca7105584821ce614
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-07-12 23:42:09 -0300

    Configurando o EditorConfig

--- src/routes/dashboard/Dashboard.tsx
+++ src/routes/dashboard/Dashboard.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState }from "react";
 
 import { FiChevronRight } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg";
 import { Title, Form, Repositories } from "./dashboard.styles";
 
+import api from "../../services/api";
+
 const Dashboard: React.FC = () => {
+
+  const [repositories, setRepositories] = useState([]);
+
+  function handleAddRepository(){
+    
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer"/>
@@ -30,44 +39,6 @@ const Dashboard: React.FC = () => {
           <FiChevronRight size={20} />
         </a>
 
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/6994181?s=460&u=3cbcbbcce69d20b7547b9d8520df7858e5f6e40f&v=4"
-            alt= "Renato W Schlogel"  
-          />
-          <div>
-            <strong>renatoschlogel/be-the-hero</strong>
-            <p>Be The Hero</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/6994181?s=460&u=3cbcbbcce69d20b7547b9d8520df7858e5f6e40f&v=4"
-            alt= "Renato W Schlogel"  
-          />
-          <div>
-            <strong>renatoschlogel/be-the-hero</strong>
-            <p>Be The Hero</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/6994181?s=460&u=3cbcbbcce69d20b7547b9d8520df7858e5f6e40f&v=4"
-            alt= "Renato W Schlogel"  
-          />
-          <div>
-            <strong>renatoschlogel/be-the-hero</strong>
-            <p>Be The Hero</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
       </Repositories>
 
     </>
