40e8112ca7467bbc8fa6adac454d52856048d51d
Renato Welinton Schlogel <rschlogel@hotmail.com>
2020-07-15 01:23:51 -0300

    Criando a aplicaçao - Lidando com erros

--- src/routes/dashboard/Dashboard.tsx
+++ src/routes/dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable camelcase */
 import React, { useState, FormEvent } from 'react';
 
 import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
-import { Title, Form, Repositories } from './dashboard.styles';
+import { Title, Form, Repositories, Error } from './dashboard.styles';
 
 import api from '../../services/api';
 
@@ -18,16 +19,28 @@ interface Repository {
 
 const Dashboard: React.FC = () => {
   const [newRepository, setNewRepository] = useState('');
+  const [inputError, setInputError] = useState('');
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   async function handleAddRepository(
     event: FormEvent<HTMLFormElement>,
   ): Promise<void> {
     event.preventDefault(); // não recarregar
-    const response = await api.get(`repos/${newRepository}`);
-    const repository = response.data;
-    setRepositories([...repositories, repository]);
-    setNewRepository('');
+    setInputError('');
+
+    if (!newRepository) {
+      setInputError('Digite o repositório!');
+      return;
+    }
+
+    try {
+      const response = await api.get(`repos/${newRepository}`);
+      const repository = response.data;
+      setRepositories([...repositories, repository]);
+      setNewRepository('');
+    } catch (error) {
+      setInputError('Erro ao procurar o repositório.');
+    }
   }
 
   return (
@@ -35,7 +48,7 @@ const Dashboard: React.FC = () => {
       <img src={logoImg} alt="Github Explorer" />
       <Title>Olá</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepository}
           onChange={e => setNewRepository(e.target.value)}
@@ -44,24 +57,23 @@ const Dashboard: React.FC = () => {
         <button type="submit">Pesquisar</button>
       </Form>
 
-      <Repositories>
+      {inputError && <Error>{inputError}</Error>}
 
+      <Repositories>
         {repositories.map(repository => (
           <a key={repository.full_name} href="teste">
-          <img
-            src={repository.owner.avatar_url}
-            alt={repository.owner.login}
-          />
-          <div>
-            <strong>{repository.full_name}</strong>
-            <p>{repository.description}</p>
-          </div>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
+            <FiChevronRight size={20} />
           </a>
-
         ))}
-
       </Repositories>
     </>
   );
